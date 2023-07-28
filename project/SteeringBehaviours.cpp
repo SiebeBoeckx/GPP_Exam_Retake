ec61a79185ab75b4f52c4f346ba2b76bf69f8f1d
Blackdod <siebe.boeckx@gmail.com>
2023-07-28 14:54:59 +0200

    Copied steering behaviour
    Commented wander since I won't be using it (probably) in the AI setup I'm planning

---
+++ project/SteeringBehaviours.cpp
@@ -0,0 +1,125 @@
+#include "stdafx.h"
+#include "SteeringBehaviours.h"
+#include "EliteMath/EVector2.h"
+#include "Box2D/Collision/Shapes/b2CircleShape.h"
+
+SteeringPlugin_Output Arrive::CalculateSteering(float deltaT, AgentInfo* pAgent)
+{
+	SteeringPlugin_Output steering = {};
+	steering.AutoOrient = false;
+
+	steering.LinearVelocity = m_Target.Location - pAgent->Position;
+	steering.LinearVelocity.Normalize();
+
+	const Elite::Vector2 toTarget{ m_Target.Location - pAgent->Position };
+	const float distanceSquared{ toTarget.MagnitudeSquared() };
+
+	if (distanceSquared < m_StopRadius * m_StopRadius)
+	{
+		steering.LinearVelocity = Elite::Vector2{ 0.f,0.f };
+		return steering;
+	}
+
+	if (distanceSquared < m_SlowRadius * m_SlowRadius)
+	{
+		steering.LinearVelocity *= pAgent->MaxLinearSpeed * distanceSquared / (m_SlowRadius * m_SlowRadius);
+	}
+	else
+	{
+		steering.LinearVelocity *= pAgent->MaxLinearSpeed;
+	}
+
+	//LookAt
+	const Elite::Vector2 desiredVector = Elite::Vector2(m_Target.Location - pAgent->Position);
+	const Elite::Vector2 lookVector{ std::cosf(pAgent->Orientation),std::sinf(pAgent->Orientation) };
+
+	float angle{ Elite::AngleBetween(desiredVector, lookVector) };
+
+	if (!(fabsf(Elite::AngleBetween(lookVector, desiredVector)) < 0.1f))
+	{
+		if (Elite::AngleBetween(lookVector, desiredVector) > 0)
+		{
+			steering.AngularVelocity = pAgent->MaxAngularSpeed;
+		}
+		else
+		{
+			steering.AngularVelocity = -pAgent->MaxAngularSpeed;
+		}
+
+	}
+
+	return steering;
+}
+
+SteeringPlugin_Output Seek::CalculateSteering(float deltaT, AgentInfo* pAgent)
+{
+	SteeringPlugin_Output steering = {};
+	steering.AutoOrient = false;
+
+	steering.LinearVelocity = m_Target.Location - pAgent->Position;
+	steering.LinearVelocity.Normalize();
+	steering.LinearVelocity *= pAgent->MaxLinearSpeed;
+
+	steering.AngularVelocity = pAgent->MaxAngularSpeed;
+
+	return steering;
+}
+
+//SteeringPlugin_Output Wander::CalculateSteering(float deltaT, AgentInfo* pAgent)
+//{
+//	SteeringPlugin_Output steering = {};
+//	b2CircleShape wanderCircle{};
+//	//const Elite::Vector2 directionVector{ pAgent->GetPosition() - m_Target.Position}; //desired direction
+//	Elite::Vector2 circleCenter = pAgent->LinearVelocity.GetNormalized() * m_OffsetDistance + pAgent->Position; //circle center
+//	wanderCircle.m_p.Set(circleCenter.x, circleCenter.y);
+//	wanderCircle.m_radius = m_Radius;
+//	
+//	const float addedAngle{ Elite::randomFloat(-m_MaxAngleChange, m_MaxAngleChange) }; //random angle withing maxAngle interval
+//	
+//	m_WanderAngle += addedAngle; //add direction change
+//	//std::cout << m_WanderAngle << '\n';
+//	
+//	const Elite::Vector2 targetPoint{ circleCenter + Elite::Vector2{cosf(m_WanderAngle), sinf(m_WanderAngle)}.GetNormalized() * m_Radius }; //calculate target point
+//	
+//	m_Target.Location = targetPoint;
+//	steering = Seek::CalculateSteering(deltaT, pAgent);
+//	
+//	return steering;
+//}
+
+SteeringPlugin_Output Flee::CalculateSteering(float deltaT, AgentInfo* pAgent)
+{
+	SteeringPlugin_Output steering = {};
+	steering.AutoOrient = false;
+
+	const Elite::Vector2 toTarget{ m_Target.Location - pAgent->Position };
+	const float distanceSquared{ toTarget.MagnitudeSquared() };
+
+	if (distanceSquared < m_FleeRadius * m_FleeRadius)
+	{
+		steering.LinearVelocity = pAgent->Position - m_Target.Location;
+		steering.LinearVelocity.Normalize();
+		steering.LinearVelocity *= pAgent->MaxLinearSpeed;
+	}
+
+	//LookAt
+	const Elite::Vector2 desiredVector = Elite::Vector2(m_Target.Location - pAgent->Position);
+	const Elite::Vector2 lookVector{ std::cosf(pAgent->Orientation),std::sinf(pAgent->Orientation) };
+
+	float angle{ Elite::AngleBetween(desiredVector, lookVector) };
+
+	if (!(fabsf(Elite::AngleBetween(lookVector, desiredVector)) < 0.1f))
+	{
+		if (Elite::AngleBetween(lookVector, desiredVector) > 0)
+		{
+			steering.AngularVelocity = pAgent->MaxAngularSpeed;
+		}
+		else
+		{
+			steering.AngularVelocity = -pAgent->MaxAngularSpeed;
+		}
+
+	}
+
+	return steering;
+}
---
+++ project/SteeringBehaviours.h
@@ -0,0 +1,87 @@
+#pragma once
+#include "Exam_HelperStructs.h"
+
+
+#pragma region **ISTEERINGBEHAVIOR** (BASE)
+class ISteeringBehavior
+{
+public:
+	ISteeringBehavior() = default;
+	virtual ~ISteeringBehavior() = default;
+
+	virtual SteeringPlugin_Output CalculateSteering(float deltaT, AgentInfo* pAgent) = 0;
+
+	//Seek Functions
+	void SetTarget(const EntityInfo& target) { m_Target = target; }
+	void SetTarget(const Elite::Vector2& target) { m_Target.Location = target; }
+	void SetFleeRadius(float radius) { m_FleeRadius = radius; };
+
+	template<class T, typename std::enable_if<std::is_base_of<ISteeringBehavior, T>::value>::type* = nullptr>
+	T* As()
+	{
+		return static_cast<T*>(this);
+	}
+
+protected:
+	EntityInfo m_Target;
+	float m_FleeRadius;
+};
+#pragma endregion
+
+class Arrive : public ISteeringBehavior
+{
+public:
+	Arrive() = default;
+	virtual ~Arrive() = default;
+
+	//Arrive Behaviour
+	SteeringPlugin_Output CalculateSteering(float deltaT, AgentInfo* pAgent) override;
+
+	void SetSlowRadius(float size) { m_SlowRadius = size; };
+	void SetTargetRadius(float distance) { m_StopRadius = distance; };
+
+private:
+	float m_SlowRadius{ 2.f };
+	float m_StopRadius{ 2.f };
+};
+
+class Seek : public ISteeringBehavior
+{
+public:
+	Seek() = default;
+	virtual ~Seek() = default;
+
+	//Seek Behaviour
+	SteeringPlugin_Output CalculateSteering(float deltaT, AgentInfo* pAgent) override;
+};
+
+//class Wander : public Seek
+//{
+//public:
+//	Wander() = default;
+//	virtual ~Wander() = default;
+//
+//	void SetWanderOffset(float offset) { m_OffsetDistance = offset; }
+//	void SetWanderRadius(float radius) { m_Radius = radius; }
+//	void SetMaxAngleChange(float rad) { m_MaxAngleChange = rad; }
+//
+//	//Arrive Behaviour
+//	SteeringPlugin_Output CalculateSteering(float deltaT, AgentInfo* pAgent) override;
+//
+//	float m_OffsetDistance{ 10.0f }; // Offset (agent direction)
+//	float m_Radius{ 4.0f }; // Wander radius
+//	float m_MaxAngleChange{ Elite::ToRadians(30) }; // Max wander angle change per frame
+//	float m_WanderAngle{}; //Internal
+//	//const float m_TimeBetweemDirectionChange{0.5f};
+//	//float m_CurrentTime{0};
+//};
+
+class Flee : public ISteeringBehavior
+{
+public:
+	Flee() = default;
+	virtual ~Flee() = default;
+
+	//Flee Behaviour
+	SteeringPlugin_Output CalculateSteering(float deltaT, AgentInfo* pAgent) override;
+};
